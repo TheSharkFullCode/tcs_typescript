7c4735f0d5efb50c48578115e3e87ebfc36eafaf
TheSharkFullCode <agedjoscar@hotmail.com>
2024-03-31 09:57:23 +0200

    donde

--- clase0/dist/index.js
+++ clase0/dist/index.js
@@ -3,3 +3,33 @@ function hola(name) {
     console.log(`binevenido ${name}`);
 }
 hola("oscar");
+
+let myname = "Oscar";
+let edad = 18;
+
+function Saludar(myname) {
+    return `bienvenido ${myname}`;
+}
+
+// -------------------------------------------------------
+// tipos de datos ------------------------->
+
+let numero = 10;
+let texto = "bienvenido";
+let boolean = true;
+let listanumber = [1,2,3,4,5];
+let objeto = {nombre:"oscar",edad:30};
+
+function Myfunction() {
+    console.log('welcome');    
+}
+
+let persona = {
+    nombre: 'danilo',
+    edad:18,
+    ciudad:'madrid'
+}
+console.log(persona.ciudad);
+persona.edad = 33; //asi modificamos el valor actual del objeto por el nuevo valor
+
+persona.trabajo = "programador" // asi agregamos una nueva propiedad al objeto.
--- clase0/src/index.ts
+++ clase0/src/index.ts
@@ -3,3 +3,49 @@ function hola(name:string){
     
 }
 hola("oscar")
+
+
+let myname:string = "Oscar";
+let edad:number = 18;
+
+function Saludar(myname:string):string{
+    
+    return `bienvenido ${myname}`;
+
+}
+
+const Myedad=(edad:number):number =>{
+    return edad;
+
+}
+Myedad(12);
+
+// ------------------------------------------
+
+let numero: number = 10;
+let string: string = "hello world";
+let isTrue: boolean = true;
+
+let listaofNumber: number[] = [1,2,3,4,5];
+let tupla: [string,number]= ["oscar",18];
+
+enum Colores {Rojo,Verde,Azul};
+let colors: Colores = Colores.Verde;
+let cualquierdato: any = "cualquier expresion"
+
+
+// ----------------------------------------
+// funcion que no devuelve nada: void
+ function saludos():void{
+        console.log(`hola`);
+        
+
+ }
+// variable que pueden ser indefinaidas o nulas
+ let nulo: null = null;
+ let undefine: undefined = undefined;
+//  -------------------------------------------------------->
+
+
+
+
